421898a59abb2f92f593bfe773dd0f92c3e87b3b
elia <raiedaleve@gmail.com>
2026-05-22 02:51:19 +0300

    revers and count_upper functions

---
+++ Pointers/PointersDay5.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int count_upper(char *str)
+{
+    int count = 0;
+    while (*str)
+    {
+        if (*str >= 'A' && *str <= 'Z')
+        {
+            count++;
+        }
+        str++;
+    }
+    return count;
+}
+
+void reverse(char *str)
+{
+    char *start = str;
+    char *end = str;
+    while (*end)
+    {
+        end++;
+    }
+    end--;
+
+    while (start < end)
+    {
+        char tmp = *start;
+        *start = *end;
+        *end = tmp;
+        start++;
+        end--;
+    }
+}
+
+int main()
+{
+    char str[] = "HELLO";
+
+    reverse(str);
+
+    char *p = str;
+    while (*p)
+    {
+        printf("%c", *p);
+        p++;
+    }
+    printf("\n");
+    return 0;
+}
